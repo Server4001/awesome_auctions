79c1e790c10ddd5249b1951f47df72aa812d478c
Brice Bentler <server4001@gmail.com>
2015-02-28 19:47:32 -0800

    Adds product scaffold and Twitter Bootstrap

--- awesome_auctions/db/schema.rb
+++ awesome_auctions/db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150228231019) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "image",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
 end
