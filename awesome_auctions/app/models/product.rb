b9d8cd4775d2fedd8a48de5148e30133a86ee9b5
Brice Bentler <server4001@gmail.com>
2015-03-01 12:07:42 -0800

    Adds auctions model and controller

--- awesome_auctions/app/models/product.rb
+++ awesome_auctions/app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ActiveRecord::Base
+  # Class constants
+  URL_REGEX = /\A(https?:\/\/)([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?\z/
+
+  # Model associations
+  belongs_to :user
+
+  # Model validations
+  validates :name, presence: true
+  validates :image, presence: true, format: {with: URL_REGEX, message: "must be a valid URL starting with: http://...", multiline: true}
 end
--- awesome_auctions/config/routes.rb
+++ awesome_auctions/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :products
+  devise_for :users
+  resources :products do
+    resources :auctions, only: [:create]
+  end
   root 'static_pages#home'
 end
